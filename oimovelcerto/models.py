6aa98e530f6fdff3a50e8cedcc2485bf38e50249
brupohlmann <brupohlmann@gmail.com>
2018-07-07 09:20:24 -0300

    arquivos static funcionando

--- oimovelcerto/models.py
+++ oimovelcerto/models.py
@@ -8,4 +8,6 @@ class cadastro(models.Model):
     CEP = models.CharField(max_length=9)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     descricao = models.CharField(max_length=200)
-    foto = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=120,)
+    foto = models.ImageField(upload_to='media/%Y/%m/%d', height_field=None, width_field=None, max_length=120,)
+
+# nao ta funcionando o upload de imagens!
--- oimovelcerto/views.py
+++ oimovelcerto/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import cadastro
-from .forms import CadastroForm
+from .form import CadastroForm
 
 # Create your views here.
 def home(request):
     data = {}
+
     return render(request, 'oimovelcerto/home.html', {})
 
 
 
 def cadastro(request):
     data = {}
-    form = CadastroForm(request.Post or NONE)
-    data['forms'] = form
+    form = CadastroForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/thanks/')
+
+    data['form'] = form
     return render(request, 'oimovelcerto/cadastro.html', data)
